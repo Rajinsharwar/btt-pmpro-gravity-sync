92548338de0c6d1d4729cc8afbc349a422150083
Rajinsharwar <rajinsharwar@gmail.com>
2023-11-16 20:50:20 -0500

    Adding version 1.2

--- pmpro-gravity-sync.php
+++ pmpro-gravity-sync.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
  * Plugin Name: PmPro and Gravity Form Sync
  * Description: WordPress plugin for Big Tomato Tech to sync between Gravity Form and Paid Membership pro.
- * Version: 1.0
+ * Version: 1.2
  * Requires at least: 6.0
  * Author: Rajin Sharwar
  * Author URI: https://profiles.wordpress.org/rajinsharwar
@@ -80,21 +81,26 @@ function assign_pmpro_level_on_activation($user_id, $user_data, $entry) {
 add_action('gform_user_registered', 'assign_pmpro_level_on_activation', 10, 3);
 
 // Implement the following functions to obtain Stripe information.
-function get_total_amount_from_stripe($entry_id, $form_id) {
+function get_total_amount_from_stripe( $entry_id, $form_id ) {
     global $wpdb;
-    
-    // Specify the table name for Gravity Forms entries.
-    $entry_table = $wpdb->prefix . 'gf_entry';
 
-    // Query the database to get the transaction ID based on the entry ID.
-    $payment_amount = $wpdb->get_var($wpdb->prepare(
-        "SELECT payment_amount FROM $entry_table WHERE id = %d AND form_id = %d",
+    // Define the table name
+    $table_name = $wpdb->prefix . 'gf_pmpro_sync';
+
+    // Query to retrieve the grand total amount based on entry ID and form ID
+    $query = $wpdb->prepare(
+        "SELECT grandtotal_amount FROM $table_name WHERE entry_id = %d AND form_id = %d",
         $entry_id,
         $form_id
-    ));
+    );
 
-    return $payment_amount;
+    // Get the result from the database
+    $total_amount = $wpdb->get_var( $query );
+
+    // Return the total amount
+    return $total_amount;
 }
+
 
 function get_last_four_digits_from_stripe($entry_id, $form_id) {
     global $wpdb;
@@ -126,3 +132,92 @@ function get_transaction_id_from_stripe($entry_id, $form_id) {
 
     return $transaction_id;
 }
+
+/*
+*
+*Get the total amount charged by Stripe.
+*
+*/
+use Gravity_Forms\Gravity_Forms\Orders\Summaries\GF_Order_Summary;
+
+function log_stripe_total_amount( $entry, $form ) {
+    // Check if the form ID is 64
+    if ( $form['id'] == 64 ) {
+        // Get the total amount charged by Stripe
+        $order_summary = GF_Order_Summary::render( $form, $entry );
+
+        // Extract grandtotal_amount from HTML using regex
+        $grandtotal_amount = get_grandtotal_amount_from_html( $order_summary );
+
+        // Save the grandtotal amount in the database
+        save_grandtotal_to_database( $form['id'], $entry['id'], $grandtotal_amount );
+    }
+}
+
+add_action( 'gform_after_submission_64', 'log_stripe_total_amount', 10, 2 );
+
+function get_grandtotal_amount_from_html( $html ) {
+    // Use regex to extract the grandtotal amount from the HTML
+    $pattern = '/<td class="grandtotal_amount">\$([0-9.,]+)<\/td>/';
+    preg_match( $pattern, $html, $matches );
+
+    // Return the matched grandtotal amount or an empty string if not found
+    return isset( $matches[1] ) ? $matches[1] : '';
+}
+
+// Function to save grandtotal to the database
+function save_grandtotal_to_database( $form_id, $entry_id, $grandtotal_amount ) {
+    global $wpdb;
+
+    // Define the table name
+    $table_name = $wpdb->prefix . 'gf_pmpro_sync'; // Replace 'your_custom_table_name' with your desired table name
+
+    // Check if the table exists, if not, create it
+    if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) != $table_name ) {
+        $charset_collate = $wpdb->get_charset_collate();
+        $sql = "CREATE TABLE $table_name (
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            form_id mediumint(9) NOT NULL,
+            entry_id mediumint(9) NOT NULL,
+            grandtotal_amount varchar(255) NOT NULL,
+            PRIMARY KEY (id)
+        ) $charset_collate;";
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+    }
+
+    // Insert data into the table
+    $wpdb->insert(
+        $table_name,
+        array(
+            'form_id'          => $form_id,
+            'entry_id'         => $entry_id,
+            'grandtotal_amount'=> $grandtotal_amount,
+        )
+    );
+}
+
+/*
+* Add PMPro billing fields to the edit user profile page.
+*/
+function add_billing_fields_to_profile()
+{
+    
+    //check for register helper
+    if(!function_exists("pmprorh_add_registration_field"))
+        return;
+    
+    //define the fields
+    $fields = array();
+    $fields[] = new PMProRH_Field("pmpro_baddress1", "text", array("label"=>"Billing Address 1", "size"=>40, "profile"=>true, "required"=>false));
+    $fields[] = new PMProRH_Field("pmpro_baddress2", "text", array("label"=>"Billing Address 2", "size"=>40, "profile"=>true, "required"=>false));
+    $fields[] = new PMProRH_Field("pmpro_bcity", "text", array("label"=>"Billing City", "size"=>40, "profile"=>true, "required"=>false));
+    $fields[] = new PMProRH_Field("pmpro_bstate", "text", array("label"=>"Billing State", "size"=>10, "profile"=>true, "required"=>false));
+    $fields[] = new PMProRH_Field("pmpro_bzipcode", "text", array("label"=>"Billing Postal Code", "size"=>10, "profile"=>true, "required"=>false));
+    $fields[] = new PMProRH_Field("pmpro_bphone", "text", array("label"=>"Billing Phone", "size"=>40, "profile"=>true, "required"=>false)); 
+    
+    //add the fields into a new checkout_boxes are of the checkout page
+    foreach($fields as $field)
+        pmprorh_add_registration_field("profile", $field);
+}
+add_action("init", "add_billing_fields_to_profile");
